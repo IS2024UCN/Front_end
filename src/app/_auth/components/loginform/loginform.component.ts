22c0af1c30ec48abf5c3c7f60be0c6151e0b7f77
VichoFL <vicente.flores@alumnos.ucn.cl>
2024-10-31 14:45:11 -0300

    feat(): agrega codigo y archivos
    Se agregaron archivos, componentes y codigo para la parte de login

--- src/app/_auth/components/loginform/loginform.component.ts
+++ src/app/_auth/components/loginform/loginform.component.ts
@@ -1,9 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthServiceService } from '../../service/auth-service.service';
 
 @Component({
   selector: 'auth-loginform',
   standalone: true,
   templateUrl: './loginform.component.html',
-  styleUrls: ['./loginform.component.css']  // Cambia a "styleUrls"
+  styleUrls: ['./loginform.component.css'],  // Cambia a "styleUrls"
+  providers: [AuthServiceService]
 })
-export class LoginformComponent {}
+export class LoginformComponent {
+
+  form!: FormGroup;
+  loginAlert: boolean = false;
+
+  error: boolean = false;
+  errorMessage: string[] = [];
+  
+  private authService = Inject(AuthServiceService);
+
+  constructor(private fb: FormBuilder, private router: Router ) {
+
+   }
+
+   formulario(): void {
+    this.form = this.fb.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  get emailValidate() {
+    return this.form.get('email')?.invalid && this.form.get('email')?.touched;
+  }
+
+  get passwordValidate() {
+    return this.form.get('password')?.invalid && this.form.get('password')?.touched;
+  }
+
+  async login(){
+    if(this.form.invalid){
+      Object.values(this.form.controls).forEach(control => {
+        control.markAsTouched();
+      });
+      return; 
+    }
+    this.loginAlert = true;
+
+    try {
+      const response = await this.authService.login(this.form.value);
+      //TODO validar si el usuario existe en response
+      if(response.data.user){
+        this.authService.setClientLogger(response.data.user);
+        this.router.navigate(['/']);
+      }else{
+        console.log('Error en ell complemento del login [Login Form]: ', response);
+        this.error = true;
+        this.errorMessage.push('Error de autenticación');
+      }
+
+      //TODO si el usuario no existe
+      
+    } catch (error) {
+      this.error = true;
+      this.errorMessage.push('Error al iniciar sesión');
+    }
+  }
+
+}
--- src/app/_auth/service/auth-service.service.ts
+++ src/app/_auth/service/auth-service.service.ts
@@ -1,18 +1,46 @@
-import { Injectable } from '@angular/core';
-import { User } from '../interfaces/ResponseAPI';
+import { Inject, Injectable } from '@angular/core';
+import { ResponseAPILogin, User } from '../interfaces/ResponseAPI';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
-
-  private baseUrl = 'http://localhost:8000/api/'
-
+  private baseUrl = 'http://localhost:8000/api';
   public errors: string[] = [];
   private userLogged: User | null = null;
 
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  async login(form: any): Promise<ResponseAPILogin> {
+    try {
+      const data = await firstValueFrom(this.http.post<ResponseAPILogin>(`${this.baseUrl}/login`, form, this.crearHeaders()));
+      return Promise.resolve(data);
+    } catch (error) {
+      console.log('Error en el servicio del login [Auth Service]: ', error);
+      let e = error as HttpErrorResponse;
+      this.errors.push(e.message || 'Error desconocido');
+      return Promise.reject(this.errors);
+    }
+  }
 
-  login(algo:any){}
+  setClientLogger(user: User ): void{
+    this.userLogged = user;
+    localStorage.setItem('User', JSON.stringify(user));
+  }
+
+   logout(): void{
+    this.userLogged = null;
+    localStorage.removeItem('User');
+   }
+
+  crearHeaders(){
+    return{
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    }
+  }
 }
