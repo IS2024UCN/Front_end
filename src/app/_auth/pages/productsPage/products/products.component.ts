ec0517117cf09819cc7c254323be0e125bf06a3e
VichoFL <vicente.flores@alumnos.ucn.cl>
2024-12-12 18:48:39 -0300

    feat(): Implementacion de barra de busqueda para productos

--- src/app/_auth/pages/productsPage/products/products.component.ts
+++ src/app/_auth/pages/productsPage/products/products.component.ts
@@ -3,30 +3,47 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthServiceService } from '../../../service/auth-service.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css', // Sin providers aquí
+  styleUrl: './products.component.css',
   providers: [AuthServiceService]
 })
 export class ProductsComponent implements OnInit {
   authService = inject(AuthServiceService);
-  products: any = [];
+  products: any[] = []; // Lista original de productos
+  filteredProducts: any[] = []; // Lista filtrada que se muestra en la tabla
+  searchTerm: string = ''; // Almacena el término de búsqueda del usuario
 
   constructor() {}
 
   ngOnInit(): void {
+    // Obtener la lista de productos al inicializar el componente
     this.authService
       .getProducts()
       .then((data: any) => {
         console.log('Data: ', data);
         this.products = data.data;
+        this.filteredProducts = this.products; // Inicializa la lista filtrada con todos los productos
       })
       .catch((error: any) => {
         console.error('Error fetching products:', error);
       });
+  }
+
+  /**
+   * Filtra los productos según el término ingresado en la barra de búsqueda.
+   */
+  filterProducts(): void {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredProducts = this.products.filter(product =>
+      product.ISBN.toLowerCase().includes(term) ||
+      product.title.toLowerCase().includes(term) ||
+      product.creator.toLowerCase().includes(term)
+    );
   }
 }
